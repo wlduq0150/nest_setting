7e3d6e97e89db553e9e9f2563f2316f679712a28
wlduq0150 <wlduq0150@gmail.com>
2023-12-20 16:51:00 +0900

    폴더 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigProjectModule } from './config/config.module';
 import { TypeormModule } from './typeorm/typeorm.module';
+import { ShowModule } from './show/show.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [ConfigProjectModule, TypeormModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigProjectModule,
+        TypeormModule.forRoot(),
+        ShowModule,
+        AuthModule,
+        UserModule,
+        BookModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
